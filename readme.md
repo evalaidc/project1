a74d4a698c5d857146a88b4811fa79d7972cd1ce
Eva Lai <evalai.dc@gmail.com>
2017-01-31 21:23:41 -0500

    new readme file

---
+++ readme.md
@@ -0,0 +1,12 @@
+Explanations of the technologies used, the approach taken, installation instructions, unsolved problems, etc.
+
+User Stories:
+As a user, I should be able to move the cookies/disks left and right so that I can functionally completely the game.
+
+As a user, I should not be able restrict movement of cookies so that I cannot put cookies/disks on top of smaller cookies/disks.
+
+As a user, I should be able to drag cookies to each spot moving it fluidly and deselecting that allows me to complete the task more quickly and efficiently.
+
+As a user, I should be able to understand where the limitation of the board are so that I do not try to click outside of the playing field that will produce no result.
+
+As a user, I should
---
+++ script.js
@@ -0,0 +1,44 @@
+//Pseudocode
+var selection = false;
+// select the cookie to be moved
+$('.post').on('click', function() {
+  // a cookie is selected
+  if (selection == true) {
+    // run the conditional
+      // value of selected box
+    var value1 = $('.active').text();
+      // # in selected Post's top cookie
+    var value2 = $(this).children(':first-child').text();
+    // checks to see if there is a cookie at the place already
+    // or if the cookie is small enough to go on top.
+    if ($(this).children().length === 0 || +value1 < +value2) {
+      $('.active').prependTo($(this))
+      $('.active').removeClass('active');
+      selection = false;
+    }
+    // alerts you if a cookie is too large to go on the top cookie of the seleted place
+    else  {
+      $('.active').removeClass('active');
+      selection = false;
+    }
+  }
+  // if a cookie was not selected
+  else {
+    $(this).children(':first').addClass('active');
+    selection = true;
+  }
+  // if player gets all cookies in order to cookie monster.
+  // if ($('#post3').children().length === 4) {
+  //   setInterval(wonAlert(), 2000);
+  //   clearInterval()
+
+  // var won = setInterval(wonAlert(),5000);
+  setTimeout(function(){
+    if ($('#post3').children().length === 4) {
+    alert('You Won! The cookie monster thanks you for your help!')
+    }
+},3000)
+    // clearInterval();
+  // }
+
+});
